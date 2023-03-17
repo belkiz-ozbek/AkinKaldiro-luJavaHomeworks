fccdd840fe3815b7e158e465e652f717ff6f3774
belkiz-ozbek <74767722+belkiz-ozbek@users.noreply.github.com>
2023-03-17 21:01:20 +0300

    Add files via upload

---
+++ SelamTest.java
@@ -0,0 +1,16 @@
+ public  class SelamTest {
+    
+    public static void main(String[] args) {
+    
+        Selam nesne = new Selam();
+        
+        String cevap = nesne.selamSoyle("Belkız");
+        String cvp = nesne.selamSoyle("");
+        System.err.println(cevap) ;
+        System.out.println(cvp);
+        
+    
+    }
+    
+}
+
